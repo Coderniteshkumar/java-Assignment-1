5d43745952e036bf11fc3a3ff96def6e09fb57e5
Nitesh Coder <152203678+Coderniteshkumar@users.noreply.github.com>
2024-08-25 00:20:54 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,63 @@
+package assignment1;
+
+public class operators {
+public static void main(String args[]) {
+//1.Arithmetic oprators[+,-,*,/,%]
+//int a=10,b=5;
+//System.out.println("sum="+(a+b));
+//System.out.println("sub="+(a-b));
+//System.out.println("mul="+a*b);
+//System.out.println("div="+a/b);
+//System.out.println("modulas="+a%b);
+
+//2.Assignment oprators[=,++,--]
+
+//3.comparison oprators or relational oprater[==,>,>=,<,<=,!=] 
+int a=10,b=5;
+//if(a==b) {
+//	System.out.print("A and B both are equal");
+//}
+//
+//else {
+//	System.out.print(" not equal");
+//}
+
+//if(a>b) {
+//	System.out.print("a is grater");
+//}
+//else {
+//	System.out.print("b is grater");
+//}
+
+//4.logical oprators[&& || !=]
+
+//int cash=1;
+//if(cash>=20 && cash<=50) {
+//	System.out.print("buy one product");
+//}
+//else if(cash>50 && cash<=100) {
+//	System.out.print("buy any product");
+//}	
+//else {
+//	System.out.print("not buy product");
+//}
+
+//int cash=80;
+//if(cash>=20 || cash<=60) {
+//	System.out.print("buy one thing");
+//}
+//
+//else {
+//	System.out.print("not buy");
+//}
+
+//int cash=50;
+//if(cash>=10 != cash<=40) {
+//	System.out.print("buy smoll product");
+//}
+//else {
+//	System.out.print("not buy");
+//}
+
+}
+}
---
+++ takeuser_input.java
@@ -0,0 +1,14 @@
+package assignment1;
+import java.util.Scanner;
+public class takeuser_input {
+public static void main(String args []) {
+ Scanner sc=new Scanner(System.in);
+ System.out.print("enter two number");
+ int a,b,sum;
+ a=sc.nextInt();
+ b=sc.nextInt();
+ sum=a+b;
+ System.out.print("sum="+sum);
+
+}
+}
